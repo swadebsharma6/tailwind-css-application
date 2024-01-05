8368f1bd3203f13699a2cdf2bbd7ff70c84e8d67
swadebsharma6 <swadebriva2012@gmail.com>
2024-01-05 21:56:31 +0600

    added tasilwind application and exploration

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,21 @@ function App() {
  
 
   return (
-    <section>
-      <h1>Hello World</h1>
-       
+    <section className="">
+    <div
+    className="bg-[url('https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg')] bg-center bg-cover bg-no-repeat h-screen"
+    >
+    <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  </div>
+    <div className="bg-gradient-to-r from-blue-300 via-purple-500 to-indigo-500 h-[500px] pt-10">
+   <div className="flex">
+    <div className="flex-1 h-20 border p-10">First Div</div>
+    <div className="h-20 flex-1 border p-10">Second Div</div>
+    <div className="h-20 flex-1 border p-10">Third Div</div>
+   </div>
+    </div>
     </section>
   )
 }
